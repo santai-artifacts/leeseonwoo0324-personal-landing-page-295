3b9f99d842c61ede8cf28c55d6f75825285c1d2e
leeseonwoo0324 <leeseonwoo0324@users.noreply.sant.ai>
2026-07-16 19:28:31 +0000

    Dev server crashed check error and fix it
    Santai-Kind: autosave
    Santai-Author: leeseonwoo0324

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
+import { join } from "path";
 
-const html = readFileSync("./public/index.html", "utf-8");
+const html = readFileSync(join(import.meta.dir, "public/index.html"), "utf-8");
 
 export default {
   port: process.env.PORT || 3000,
